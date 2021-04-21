bb1d46436013ac1aa1fa82bf8dfa894410f15cf3
Mikhail <16112000m@gmail.com>
2021-04-21 11:54:23 +0300

    - working UX in first dialog

--- PRNG-on-GF/PRNG-on-GF/PRNGonGF.cpp
+++ PRNG-on-GF/PRNG-on-GF/PRNGonGF.cpp
@@ -7,30 +7,83 @@ PRNGonGF::PRNGonGF(QWidget *parent)
     ui.setupUi(this);
     ui.a_even_layout->addLayout(a_first);
     ui.c_even_layout->addLayout(c_first);
+    ui.degree_edit->setText("1");
 }
 
-void PRNGonGF::on_degree_edit_returnPressed() {
+void remove_last_widget_from_layout(QBoxLayout* parent) {
+    auto i = parent->count();
+    auto wItem = parent->takeAt(i - 1);
+    if (wItem->widget())
+        wItem->widget()->setParent(NULL);
+    delete wItem;
+}
+
+void remove_layout(QBoxLayout* layout) {
+
+    QLayoutItem* item;
+    QLayout* sublayout;
+    QWidget* widget;
+
+    while ((item = layout->takeAt(0))) {
+        if ((sublayout = item->layout()) != 0) {}
+        else if ((widget = item->widget()) != 0) { widget->hide(); delete widget; }
+        else { delete item; }
+    }
+
+    delete layout;
+}
+
+void set_spinbox_max_value(QBoxLayout* parent, int max_degree) {
+
+    QLayoutItem* item;
+    QLayout* sublayout;
+    QSpinBox* widget;
+
+    auto count = parent->count();
+
+    for (size_t i = 0; i < count; i++)
+    {
+        item = parent->itemAt(i);
+        widget = dynamic_cast<QSpinBox*>(item->widget());
+        widget->setMaximum(max_degree);
+    }
+ 
+}
+
+QSpinBox* create_new_spinbox(int max_degree) {
+    auto newSpinBox = new QSpinBox();
+    newSpinBox->setMaximum(max_degree);
+    newSpinBox->setMinimum(0);
+    return newSpinBox;
+}
+
+void PRNGonGF::on_n_edit_returnPressed() {
     bool ok;
-    int new_degree = ui.degree_edit->text().toInt(&ok);
+    int new_n = ui.n_edit->text().toInt(&ok);
+    int max_degree = ui.degree_edit->text().toInt(&ok);
     bool use_even_odd = ui.even_checkbox->isChecked();
     if (ok == true) {
-        if(new_degree < 1)
-            ui.statusBar->showMessage("Degree can't be less than 1");
+        if(new_n < 1)
+            ui.statusBar->showMessage("N can't be less than 1");
         else {
-            ui.statusBar->showMessage("New degree: " + QString::number(new_degree));
-            int current_degree = ui.a_even_layout->count();
-            for (size_t i = current_degree; i < new_degree + 1; i++)
+            ui.statusBar->showMessage("New n: " + QString::number(new_n));
+            int current_degree = a_first->count();
+            for (size_t i = current_degree; i < new_n; i++)
             {
-                auto newSpinBox1 = new QSpinBox();
-                auto newSpinBox2 = new QSpinBox();
-                a_first->addWidget(newSpinBox1);
-                c_first->addWidget(newSpinBox2);
+                a_first->addWidget(create_new_spinbox(max_degree));
+                c_first->addWidget(create_new_spinbox(max_degree));
 
                 if (use_even_odd) {
-                    auto newSpinBox3 = new QSpinBox();
-                    auto newSpinBox4 = new QSpinBox();
-                    a_second->addWidget(newSpinBox3);
-                    c_second->addWidget(newSpinBox4);
+                    a_second->addWidget(create_new_spinbox(max_degree));
+                    c_second->addWidget(create_new_spinbox(max_degree));
+                }
+            }
+            for (size_t i = current_degree; i > new_n; i--) {
+                remove_last_widget_from_layout(a_first);
+                remove_last_widget_from_layout(c_first);
+                if (use_even_odd) {
+                    remove_last_widget_from_layout(a_second);
+                    remove_last_widget_from_layout(c_second);
                 }
             }
         }
@@ -40,11 +93,19 @@ void PRNGonGF::on_degree_edit_returnPressed() {
     }
 }
 
-void PRNGonGF::on_n_edit_returnPressed() {
+
+void PRNGonGF::on_degree_edit_returnPressed() {
     bool ok;
-    int new_n = ui.n_edit->text().toInt(&ok);
+    int new_degree = ui.degree_edit->text().toInt(&ok);
+    bool use_even_odd = ui.even_checkbox->isChecked();
     if (ok == true) {
-        ui.statusBar->showMessage("New N: " + QString::number(new_n));
+        ui.statusBar->showMessage("New degree: " + QString::number(new_degree));
+        set_spinbox_max_value(a_first, new_degree);
+        set_spinbox_max_value(c_first, new_degree);
+        if (use_even_odd) {
+            set_spinbox_max_value(a_second, new_degree);
+            set_spinbox_max_value(c_second, new_degree);
+        }
     }
     else {
         ui.statusBar->showMessage("Degree can't be a letter");
@@ -54,34 +115,21 @@ void PRNGonGF::on_n_edit_returnPressed() {
 void PRNGonGF::on_even_checkbox_stateChanged() {
     bool use_even_odd = ui.even_checkbox->isChecked();
     if (!use_even_odd) {
-        ui.statusBar->showMessage("Now single mod used");
+       ui.statusBar->showMessage("Single mod is now used");
+
        ui.a_even_layout->removeItem(a_second);
        ui.c_even_layout->removeItem(c_second);
       
-       QLayoutItem* item;
-       QLayout* sublayout;
-       QWidget* widget;
-       while ((item = a_second->takeAt(0))) {
-           if ((sublayout = item->layout()) != 0) { }
-           else if ((widget = item->widget()) != 0) { widget->hide(); delete widget; }
-           else { delete item; }
-       }
-
-       delete a_second;
-
-       while ((item = c_second->takeAt(0))) {
-           if ((sublayout = item->layout()) != 0) { }
-           else if ((widget = item->widget()) != 0) { widget->hide(); delete widget; }
-           else { delete item; }
-       }
-
-       delete c_second;
+       remove_layout(a_second);
+       remove_layout(c_second);
    
     }
     else {
+        ui.statusBar->showMessage("Even/odd mod is now used");
+
         a_second = new QHBoxLayout();
         c_second = new QHBoxLayout();
-        ui.statusBar->showMessage("Now even/odd mod used");
+
         ui.a_even_layout->addLayout(a_second);
         ui.c_even_layout->addLayout(c_second);
 
